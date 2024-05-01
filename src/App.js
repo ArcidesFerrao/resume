92f07c6a02226524214055b42fe8263f2a41c64f
ArcidesFerrao <cidesferrao@gmail.com>
2024-05-01 14:39:03 +0200

    css: theme preference

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Resume from './components/Resume';
 
 function App() {
-
   
+  const [themeData, setThemeData] = useState("dark");
+  
+
   return (
     <div className="App">
+
       <Resume />
+
     </div>
   );
 }
